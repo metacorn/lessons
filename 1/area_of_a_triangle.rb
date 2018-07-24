62004b257bfb8962a23439a9c9c5542e329bdfcd
Oleg Sachev <cbi444@gmail.com>
2018-07-24 23:16:20 +0200

    added area_of_a_triangle.rb

---
+++ 1/area_of_a_triangle.rb
@@ -0,0 +1,6 @@
+print "Please enter the triangle's length of the base: "
+base = Integer(gets.chomp)
+print "Please enter the triangle's height: "
+height = Integer(gets.chomp)
+area = base * height / 2
+puts "The area of triangle is #{area}"
