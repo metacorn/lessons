0159bad10bffa50b50bc2bde7ba35cfa5e061740
metaphorizer <41169415+metaphorizer@users.noreply.github.com>
2018-07-25 15:20:33 +0300

    Update right_triangle.rb

--- 1/right_triangle.rb
+++ 1/right_triangle.rb
@@ -12,8 +12,8 @@ b = sides[1]
 c = sides[2]
 
 if a**2 == b**2 + c**2
-	puts "The triangle is right!"
-	if a == b || b == c
-		puts "The triangle is isosceles!"
-	end
+  puts "The triangle is right!"
+  if a == b || b == c
+    puts "The triangle is isosceles!"
+  end
 end
