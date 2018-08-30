9d5b4c99929139c69364a1bccacaa4133a939f40
Oleg Sachev <cbi444@gmail.com>
2018-08-30 23:29:07 +0200

    Add Accessors module

--- course_1/part_10/accessors.rb
+++ course_1/part_10/accessors.rb
@@ -1,41 +1,35 @@
 module Accessors
   def self.included(base)
     base.extend ClassMethods
-    base.send :include, InstanceMethods
+    # base.send :include, InstanceMethods
   end
 
   module ClassMethods
     def attr_accessor_with_history(*names)
       names.each do |name|
         var_name = "@#{name}".to_sym
-        define_history(name)
-        define_setter(name, var_name)
-        define_getter(name, var_name)
+        var_history_name = "@#{name}_history".to_sym
+        define_method("#{name}_history") do
+          instance_variable_get(var_history_name)
+        end
+        define_method("#{name}=") do |value|
+          instance_variable_set(var_name, value)
+          var_history = instance_variable_get(var_history_name) || []
+          var_history << value
+          instance_variable_set(var_history_name, var_history)
+        end
+        define_method(name) { instance_variable_get(var_name) }
       end
     end
 
-    private
-
-    def define_history(name)
-      var_history_name = "@#{name}_history".to_sym
-      puts var_history_name.inspect
-      instance_variable_set(var_history_name, [])
-      define_method("#{name}_history") { instance_variable_get(var_history_name) }
-    end
-
-    def define_setter(name, var_name)
-      define_method("#{name}=") do |value|
+    def strong_attr_accessor(attr_name, attr_class)
+      var_name = "@#{attr_name}".to_sym
+      define_method(attr_name) { instance_variable_get(var_name) }
+      define_method("#{attr_name}=") do |value|
+        raise "Wrong class of value! It should be #{attr_class}!" if !value.instance_of?(attr_class)
         instance_variable_set(var_name, value)
-  #      instance_variable_get("@#{name}_history".to_sym) << value
       end
     end
-
-    def define_getter(name, var_name)
-      define_method(name) { instance_variable_get(var_name) }
-    end
-  end
-
-  module InstanceMethods
   end
 end
 
@@ -43,4 +37,5 @@ class Test
   include Accessors
 
   attr_accessor_with_history :a, :b, :c
+  strong_attr_accessor :d, Array
 end
